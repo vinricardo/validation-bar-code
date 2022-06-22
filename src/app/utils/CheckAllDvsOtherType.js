61fa01daaeddd52646f97ad9d31ebce7e4228520
Vinicius <vinicius.01deagosto@gmail.com>
2022-06-22 19:27:53 -0300

    feature: checagem digito verificador boleto de convenio

--- src/app/utils/CheckAllDvsOtherType.js
+++ src/app/utils/CheckAllDvsOtherType.js
@@ -1,51 +1,88 @@
-module.exports.checkAllDvsOtherType = (barcode) => {
-    var dvsCheck = [false, false, false, false];
-    let interval = [
-      { start: 0, end: 11 },
-      { start: 12, end: 23 },
-      { start: 24, end: 35 },
-      { start: 36, end: 47 },
-    ];
+const { refactorDigitableLine } = require("./RefactorDigitableLine");
 
-    /** check parts DV with 10 and 11 module */
-    let x = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-    interval.forEach(({ start, end }, i) => {
-      let count = 0;
-      barcode
-        .substring(start, end)
-        .split("")
-        .forEach(
-          (number, index) => (count = count + parseInt(number) * x[index])
-        );
-      let diff =
-        count % 11 == 0 || count % 11 == 1
-          ? 0
-          : count % 11 == 10
-          ? 1
-          : 11 - (count % 11);
-      diff == barcode.substring(end, end + 1) ? (dvsCheck[i] = true) : false;
-    });
-    if(dvsCheck.some(value => !value))
-        interval.forEach(({ start, end }, i) => {
-            var firstField = barcode.substring(start, end);
-            let count = 0;
-            firstField.split("").forEach((element, index) => {
-                if (index % 2 == 0) {
-                    let el = String(parseInt(element) * 2);
-                    el.length < 2
-                    ? (count = count + parseInt(el))
-                    : (count = count + parseInt(el[0]) + parseInt(el[1]));
-                } else count = count + parseInt(element);
-                
-            });
-            let diff =
-                count % 10 == 0
-                ? 0
-                : count % 10 == 10
-                ? 1
-                : 10 - (count % 10);
-            diff == barcode.substring(end, end + 1) ? (dvsCheck[i] = true) : false;
-        });
-     /** */
+module.exports.checkAllDvsOtherType = (barcode) => {
+  var dvsCheck = [false, false, false, false];
+  let interval = [
+    { start: 0, end: 11 },
+    { start: 12, end: 23 },
+    { start: 24, end: 35 },
+    { start: 36, end: 47 },
+  ];
+
+  let refactBarCode = refactorDigitableLine(barcode, false);
+  let arrayBarCode = Array.from(refactBarCode.validCode);
+
+  /** check main dv */
+  let mainCountDv = 0;
+  let multMainDV = 2;
+  var goToOtherMainCheck = false;
+
+  arrayBarCode.reverse().forEach((value) => {
+    mainCountDv = mainCountDv + multMainDV * value;
+    if (multMainDV == 9) multMainDV = 2;
+    else multMainDV++;
+  });
+  let mainDv = 11 - (mainCountDv % 11);
+  if (mainDv == 0 || mainDv == 10 || mainDv == 11) mainDv = 1;
+  if (mainDv != parseInt(refactBarCode.verificationDigit)) {
+    goToOtherMainCheck = true;
     return dvsCheck;
   }
+
+  if (goToOtherMainCheck) {
+    mainCountDv = 0;
+    arrayBarCode.forEach((value, index) => {
+      if (index % 2 == 0) {
+        let el = String(parseInt(value) * 2);
+        el.length < 2
+          ? (mainCountDv = mainCountDv + parseInt(value))
+          : (mainCountDv = mainCountDv + parseInt(el[0]) + parseInt(el[1]));
+      } else mainCountDv = mainCountDv + parseInt(value);
+    });
+    mainDv =
+      mainCountDv % 10 == 0
+        ? 0
+        : mainCountDv % 10 == 10
+        ? 1
+        : 10 - (mainCountDv % 10);
+    if (mainDv != parseInt(refactBarCode.verificationDigit)) return dvsCheck;
+  }
+  /** */
+
+  /** check parts DV with 10 and 11 module */
+  let x = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  interval.forEach(({ start, end }, i) => {
+    let count = 0;
+    barcode
+      .substring(start, end)
+      .split("")
+      .forEach(
+        (number, index) => (count = count + parseInt(number) * x[index])
+      );
+    let diff =
+      count % 11 == 0 || count % 11 == 1
+        ? 0
+        : count % 11 == 10
+        ? 1
+        : 11 - (count % 11);
+    diff == barcode.substring(end, end + 1) ? (dvsCheck[i] = true) : false;
+  });
+  if (dvsCheck.some((value) => !value))
+    interval.forEach(({ start, end }, i) => {
+      var firstField = barcode.substring(start, end);
+      let count = 0;
+      firstField.split("").forEach((element, index) => {
+        if (index % 2 == 0) {
+          let el = String(parseInt(element) * 2);
+          el.length < 2
+            ? (count = count + parseInt(el))
+            : (count = count + parseInt(el[0]) + parseInt(el[1]));
+        } else count = count + parseInt(element);
+      });
+      let diff = count % 10 == 0 ? 0 : count % 10 == 10 ? 1 : 10 - (count % 10);
+      diff == barcode.substring(end, end + 1) ? (dvsCheck[i] = true) : false;
+    });
+  /** */
+
+  return dvsCheck;
+};
--- src/app/utils/RefactorDigitableLine.js
+++ src/app/utils/RefactorDigitableLine.js
@@ -1,22 +1,26 @@
 module.exports.refactorDigitableLine = (code, isBank) => {
-    dvsPosition = isBank ? [9,20,31] : [11,23,35,47]; 
-    arrayCodeBar = Array.from(code)
-    dvsPosition.forEach((position) => {
-        arrayCodeBar[position] = "";
-    });
+  dvsPosition = isBank ? [9, 20, 31] : [11, 23, 35, 47];
+  arrayCodeBar = Array.from(code);
+  dvsPosition.forEach((position) => {
+    arrayCodeBar[position] = "";
+  });
 
-    code = arrayCodeBar.join("")
-    
-    if(isBank){
-        let moreInfosPart = code.substring(30,44);
-        let verificationDigit = code.substring(29,30)
-        let lastInfosPart = code.substring(4,29)
-        let validCode = code.substring(0,4) + moreInfosPart + lastInfosPart
-        code = code.substring(0,4) + verificationDigit + moreInfosPart + lastInfosPart
-        
-        return {code,verificationDigit, validCode}
-    } else {
-        let verificationDigit = code.substring(3,4)
-        return {code,verificationDigit}
-    }
-}
+  code = arrayCodeBar.join("");
+
+  if (isBank) {
+    let moreInfosPart = code.substring(30, 44);
+    let verificationDigit = code.substring(29, 30);
+    let lastInfosPart = code.substring(4, 29);
+    let validCode = code.substring(0, 4) + moreInfosPart + lastInfosPart;
+    code =
+      code.substring(0, 4) + verificationDigit + moreInfosPart + lastInfosPart;
+
+    return { code, verificationDigit, validCode };
+  } else {
+    let verificationDigit = code.substring(3, 4);
+    let firstInfosPart = code.substring(0, 3);
+    let lastInfosPart = code.substring(4, 44);
+    let validCode = firstInfosPart + lastInfosPart;
+    return { code, verificationDigit, validCode };
+  }
+};
